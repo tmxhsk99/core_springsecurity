9b6f890b4481f3c219a247e16e2c166004617bda
kimjuhyeon <tmxhsk99@naver.com>
2023-01-03 08:15:40 +0900

    feat : 정적 리소스(js,css,img) 보안필터 해제 설정 추가

--- src/main/java/io/security/corespringsecurity/security/configs/SecurityConfig.java
+++ src/main/java/io/security/corespringsecurity/security/configs/SecurityConfig.java
@@ -1,10 +1,12 @@
 package io.security.corespringsecurity.security.configs;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -40,5 +42,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
         .and()
                 .formLogin();
+    }
+
+    /**
+     * 정적리소스 보안필터 제거 설정
+     * @param web
+     * @throws Exception
+     */
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        // 정적 리소스 필터 제외
+        web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+
     }
 }
